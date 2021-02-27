6e9b24deeb75e1fa6d1a1161b19fc8c273d728aa
Rick Kassing <rkassing@gmail.com>
2021-02-27 15:32:03 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template
+# import necessary libraries
+from models import create_classes
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
-if __name__ == '__main__':
-    app.run(debug=True)
+
+#################################################
+# Database Setup
+#################################################
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+Pet = create_classes(db)
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
